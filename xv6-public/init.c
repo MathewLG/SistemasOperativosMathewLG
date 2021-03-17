3293fb0250f610972691428da3a21b94266ae9c4
A01635001 MathewLG <a01635001@itesm.mx>
2021-03-17 01:18:15 +0000

    Practica 2, modificacion del archivo init

--- xv6-public/init.c
+++ xv6-public/init.c
@@ -20,7 +20,7 @@ main(void)
   dup(0);  // stderr
 
   for(;;){
-    printf(1, "init: starting sh\n");
+    printf(1, "init: Amarren sus cinturones porque está arrancando el sistema operativo más perrón!\n");
     pid = fork();
     if(pid < 0){
       printf(1, "init: fork failed\n");
@@ -28,6 +28,9 @@ main(void)
     }
     if(pid == 0){
       exec("sh", argv);
+		//Cuando sustituyo el argumento por "ls" al arrancar, el sistema operativo lista todos los archivos que contiene
+		//Cuando sustituyo por "no programa" se despliega reiteradamente un mensaje de error que dice init: exec failed
+		//Cuando vuelvo a poner sh y modifico el mensaje, vuelve a la normalidad y despliega mi mensaje. 
       printf(1, "init: exec sh failed\n");
       exit();
     }
