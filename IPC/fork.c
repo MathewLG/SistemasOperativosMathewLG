74075984fbf0665870efe2a82827e4aa6d34d0b1
A01635001 MathewLG <a01635001@itesm.mx>
2021-03-24 01:46:27 +0000

    Corrige fork

--- IPC/fork.c
+++ IPC/fork.c
@@ -5,27 +5,25 @@
 int varGlobal;
 
 void signalHandler(int sig){
-  printf("Recibi segnal %d\n", sig);
-	varGlobal = sig;
+    printf("Recibi segnal %d\n", sig);
+	printf("Mi hijo termino \n");
+	varGlobal = wait(NULL);
 }
 int main(){
 	int pid = fork();
 	int pidHijoTermino;
+	signal(17, signalHandler);
 	if(pid == 0){
 		//Proceso hijo 
 		printf("Soy el proceso hijo \n");
 	}else{
-		signal(17, signalHandler);
 		while(1){
 			printf("Estoy trabajando \n");
-			sleep(.1);
-			if(varGlobal == 17){
-				break;
-			}
+			sleep(1);
 		}
 		printf("Soy el proceso padre y mi hijo es %d\n",pid);
 		//pidHijoTermino = wait(NULL);
-		printf("Mi hijo termino %d \n", pidHijoTermino);
+		printf("Mi hijo termino \n");
 	}
 	printf("Terminando\n");
 	return 0;
