91a7ab2f21a339d4cf0b22d6f997a359d0a7c468
A01635001 MathewLG <a01635001@itesm.mx>
2021-03-24 02:50:24 +0000

    Agrega min2may y contador de lineas

---
+++ IPC/cuentalineas.c
@@ -0,0 +1,14 @@
+#include <unistd.h>
+#include <ctype.h>
+#include <stdio.h>
+int main(){
+	int lineas = 0;
+	char current;
+	while(read(0,&current,1)){
+		if(current == '\n'){
+			lineas++;
+		}
+	}
+	printf("Lineas : %d\n",lineas);
+	return 0;
+}
