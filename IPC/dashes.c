821f43a233735a7be06b407d905432e204c8726c
MathewLG <mathewlopezg@gmail.com>
2021-04-07 00:45:33 +0000

    practica 6

--- IPC/dashes.c
+++ IPC/dashes.c
@@ -1,7 +1,20 @@
 #include <unistd.h>
 #include <stdio.h>
 int main(){
-	execl("/usr/bin/ls","ls","-l",NULL);
-	printf("Esto no debe verse \n");
+    int fd[2]; //Indice 0 entrada, indice 2 salida (fd significa file descriptor)
+    int pid;
+    pipe(fd);
+    pid = fork();
+    //A partir de aqui (despues del fork) tengo dos procesos clones
+    if(pid == 0){//Proceso hijo
+        close(fd[1]);
+        dup2(fd[0],STDIN_FILENO);
+        execl("/usr/bin/tr","tr","' ","-",NULL);
+    }
+    if(pid > 0){//Proceso padre
+        close(fd[0]);
+        dup2(fd[1],STDOUT_FILENO);
+        execl("/usr/bin/cat","cat",NULL);
+    }
 	return 0;
 }
