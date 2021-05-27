3281bc325a9bc5b3447eed2c796d24628b39bd14
MathewLG <mathewlopezg@gmail.com>
2021-05-27 13:36:37 +0000

    Practica 12

---
+++ files/read.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "./student.h"
+int main(int argc, char **argv){
+    char *filename = argv[1];
+    int cuantos = atoi(argv[2]);
+    FILE *forigen;
+    forigen = fopen(filename, "rb");
+    
+    for(int i = 0; i < cuantos; i++){
+        STUDENT newstudent;
+        fread(&newstudent,sizeof(newstudent),1,forigen);
+        printf("%s %s %d %d \n",newstudent.name,newstudent.lastname,newstudent.id,newstudent.semestre);
+    }
+    fclose(forigen);
+}
